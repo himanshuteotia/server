ff5f45a590e35041f6422a15c91b2f724ceb57f5
Himanshu Teotia <himanshuteotia7@gmail.com>
2018-02-06 22:53:00 +0800

    changes for index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var port = process.env.PORT || 3000;
       })
     }, function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('statusCode:', response); // Print the response status code if a response was received
       console.log('body:', body); // Print the HTML for the Google homepage.
       res.json({
         body: body,
