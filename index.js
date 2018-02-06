df7ecb1cd3fdc9cf6a39332d8176c0e287f4ec65
Himanshu Teotia <himanshuteotia7@gmail.com>
2018-02-06 14:55:00 +0800

    changes for request module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const request = require("request");
 
 app.use((request, response, next) => {
   console.log(request.headers)
@@ -11,10 +12,25 @@ app.use((request, response, next) => {
   next()
 })
 
-app.get('/', (request, response) => {
-  response.json({
-    chance: request.chance
-  })
+app.get('/', (req, res) => {
+  request.post({
+    headers: { 'content-type' : 'application/x-www-form-urlencoded' },
+    url:     'https://202.165.10.133/m2/postLogin',
+    body:    JSON.stringify({
+      "uid" : "20843287",
+      "pwd" : "abcd1234"
+    })
+  }, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.json({
+      body: body,
+      statusCode : response.statusCode,
+      response : response,
+      error : error
+    })
+  });
 })
 
 app.listen(3000,() => {
